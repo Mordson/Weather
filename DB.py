8ed5a6266d76a63ac8de8c74985d3de6b15547f1
Mordson <antolec2@gmail.com>
2022-09-09 17:00:41 +0200

    creating connection to Database and first insert

--- DB.py
+++ DB.py
@@ -2,15 +2,22 @@ import sqlite3
 import weather
 
 try:
-    conn = sqlite3.connect('WeatherDB.db')
+    conn = sqlite3.connect('WeatherDB.db') #creating database connection
     print("Połączono")
 
 except Exception as e:
-    print("error", str(e))
-    
-result = conn.execute("SELECT * FROM Temperature")
+    print("Error", str(e))
+        
 
-for row in result:
-    print(row)
-
-conn.close()
+def InsertWindSpeed(WindSpeed,Winddeg):
+    try:
+        cur = conn.cursor()
+        sql = 'INSERT INTO Wind (ID,SpeedOfWind,Angle) VALUES(' ',3,3)' #Adding values of speed and angle of wind to database 
+        cur.execute(sql)
+        conn.commit()
+        conn.close()
+        
+    except Exception as e:
+        print(e)
+     
+InsertWindSpeed(weather.WindSpeed,weather.WindDegree)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+def InsertWindSpeed(WindSpeed,Winddeg):
+#     try:
+#         cur = conn.cursor()
+#         sql = 'INSERT INTO Wind (ID,SpeedOfWind,Angle) VALUES(?,?,?)',('1',WindSpeed,Winddeg) #Adding values of speed and angle of wind to database 
+#         cur.execute(sql)
+#         conn.commit()
+#         conn.close()
+        
+#     except Exception as e:
+#         print(e)
+     
+# InsertWindSpeed(weather.WindSpeed,weather.WindDegree)

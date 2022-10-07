2dbf6bd3533f8b3af7515bc35fca6132cd61abab
mordson <antolec2@gmail.com>
2022-10-07 22:15:19 +0200

    add url to weather

--- weather.py
+++ weather.py
@@ -1,6 +1,7 @@
 import requests
 import secrets
 
+url = 'https://api.openweathermap.org/data/2.5/weather?lat=52.23&lon=21.01&appid=f96032cf675ea0097f776d08a25c4819&units=metric&lang=pl&wind.direction.value'.format()
 def GetJson(url): #this function returns requests from API
     res = requests.get(url)
     response = res.json()
@@ -18,6 +19,6 @@ def WindDegree(response):
     Winddeg = response['wind']['deg']
     return Winddeg
 
-Temperature = Temp(GetJson(secrets.url)) #temperature in Warsaw
-SpeedOfWind = WindSpeed(GetJson(secrets.url)) #speed of wind in Warsaw
-DegreeOfWind = WindDegree(GetJson(secrets.url)) #degree of wind
+Temperature = Temp(GetJson(url)) #temperature in Warsaw
+SpeedOfWind = WindSpeed(GetJson(url)) #speed of wind in Warsaw
+DegreeOfWind = WindDegree(GetJson(url)) #degree of wind
